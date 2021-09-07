75c0e3a0a33b5f7ce34469d84d0283cd0193c230
swumplurd <53883345+Swumplurd@users.noreply.github.com>
2021-09-07 17:30:08 -0500

    testeando configuraciones de gh pages

--- src/routers/RouterApp.js
+++ src/routers/RouterApp.js
@@ -4,7 +4,7 @@ import { Sidebar } from '../components/Sidebar/Sidebar'
 
 export const RouterApp = () => {
     return (
-        <Router>
+        <Router basename="/guia-estilos-sii">
             <Sidebar/>
         </Router>
     )
